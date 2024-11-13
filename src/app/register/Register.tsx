76a94f2bfcfa51831d3a1992f08b722f35649fe0
Chiara <chiaravalentinabosio@gmail.com>
2024-11-13 13:29:05 -0300

    creacion form register

---
+++ src/app/register/Register.tsx
@@ -0,0 +1,13 @@
+import RegisterForm from "@/components/Register/Register";
+
+export const Register = () => {
+  return (
+    <main>
+      <div>
+        <RegisterForm />
+      </div>
+    </main>
+  );
+};
+
+export default Register;
