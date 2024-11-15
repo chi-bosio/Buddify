4c75bbdfcdbf896b0e11b85d297a4208d060daf7
Lisandro85 <lisandrobedotti@hotmail.com>
2024-11-15 15:41:19 +0100

    Swal/PostData de Register/login

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -2,7 +2,8 @@
 import { useFormik } from "formik";
 import validationSchemaLogin from "./components/validationSchema";
 import postData from "./components/postData";
-import { toast } from "react-toastify";
+
+
 
 const LoginForm = () => {
   const formik = useFormik({
@@ -12,30 +13,8 @@ const LoginForm = () => {
     },
     validationSchema: validationSchemaLogin,
     onSubmit: async (values) => {
-      const success = await postData(values);
-      if (success) {
-        toast.success("Inicio de sesión exitoso.",{
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-        handleResetForm();
-        // Redireccionar 
-      } else {
-        toast.error("Error al iniciar sesión. Por favor, revisa los datos.",{
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      }
+        const success = await postData(values);
+        if (success) handleResetForm();
     }
 });
   const handleResetForm = () => {
@@ -44,8 +23,8 @@ const LoginForm = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 h-screen">
       <div
-        className="md:col-span-2 bg-cover bg-center h-full" 
-        style={{ backgroundImage: 'url(/assets/fondoLanding.png)' }}
+        className="md:col-span-2 bg-contain bg-center h-full" 
+        style={{ backgroundImage: 'url(/assets/fondo2.webp)' }}
       ></div>
 
       <div className="flex justify-center items-center h-full">
@@ -53,9 +32,9 @@ const LoginForm = () => {
           className="w-full h-full bg-customPalette-white p-8"
           onSubmit={formik.handleSubmit}
         >
-          <h2 className="text-4xl font-bold text-customPalette-graydark text-center mt-24 mb-24">
+          <h1 className="text-4xl font-bold text-customPalette-blue text-center mt-24 mb-24">
             Iniciar sesión
-          </h2>
+          </h1>
 
           <div className="input-group relative mb-14 w-full max-w-[400px] mx-auto">
             <input
@@ -65,12 +44,12 @@ const LoginForm = () => {
               value={formik.values.username}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              className="peer w-full min-w-[200px] h-full p-4 pt-6 border border-customPalette-black rounded-md shadow-sm focus:ring-customPalette-bluelink focus:border-customPalette-bluelightli text-customPalette-black text-xl"
+              className="peer w-full min-w-[200px] h-full p-4 pt-6 border border-customPalette-gray rounded-md shadow-sm focus:ring-customPalette-bluelink focus:border-customPalette-bluelightli text-customPalette-graydark text-xl"
               placeholder=" "
             />
             <label
               htmlFor="username"
-              className="absolute -top-3 left-2 bg-customPalette-white px-1 text-base font-medium text-customPalette-graydark mt-1"
+              className="absolute -top-3 left-2 bg-customPalette-white px-1 text-base font-medium text-customPalette-blue mt-1"
             >
               Nombre de Usuario
             </label>
@@ -89,12 +68,12 @@ const LoginForm = () => {
               value={formik.values.password}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              className="peer w-full min-w-[200px] h-full p-4 pt-6 border border-customPalette-black rounded-md shadow-sm focus:ring-customPalette-bluelink focus:border-customPalette-bluelightli text-customPalette-black text-xl"
+              className="peer w-full min-w-[200px] h-full p-4 pt-6 border border-customPalette-gray rounded-md shadow-sm focus:ring-customPalette-bluelink focus:border-customPalette-bluelightli text-customPalette-graydark text-xl"
               placeholder=" "
             />
             <label
               htmlFor="password"
-              className="absolute -top-3 left-2 bg-customPalette-white px-1 text-base font-medium text-customPalette-graydark mt-1"
+              className="absolute -top-3 left-2 bg-customPalette-white px-1 text-base font-medium text-customPalette-blue mt-1"
             >
               Contraseña
             </label>
@@ -116,27 +95,28 @@ const LoginForm = () => {
 
           <button
             type="submit"
-            className="w-1/4 min-w-[150px] bg-customPalette-orange text-customPalette-white text-lg font-semibold py-3 px-8 rounded hover:bg-customPalette-orange mt-24 mb-6 mx-auto block"
+            className="w-1/4 min-w-[150px] bg-customPalette-orange text-customPalette-white text-lg font-semibold py-3 px-8 rounded hover:bg-customPalette-orangebright mt-24 mb-6 mx-auto block"
           >
             Ingresar
           </button>
 
             <div className="mt-16 text-center">
             <p className="text-base text-customPalette-black">
-              ¿Todavia no tienes cuenta?&nbsp;
+              ¿Todavia no tenes cuenta?&nbsp;
               <a
                 href="/register"
-                className="text-customPalette-bluelightst text-lg hover:text-customPalette-bluedark"
+                className="text-customPalette-black text-lg hover:text-customPalette-bluedark underline"
               >
-                Registrarse
+                Registrate
               </a>
             </p>  
-            </div>
-            
+            </div>   
           </form>
         </div>
       </div>
   );
+
 };
+
 
 export default LoginForm;
