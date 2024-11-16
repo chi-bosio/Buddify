ac170db58cbca85d1ab0adf90d5e4faacbac70eb
Alexis-AFarias <aleexxfs99@gmail.com>
2024-11-16 20:21:20 -0300

    autenticacion front

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,18 @@ import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import AuthContextProvider from "../contexts/authContext"; 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+      <AuthContextProvider>
         <header>
-          <NavBar />
+          <NavBar/>
         </header>
         <main>{children}</main>
-        <Footer />
+        <Footer/>
         <ToastContainer
           position="top-right"
           autoClose={5000}
@@ -24,6 +26,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           draggable
           pauseOnHover
         />
+        </AuthContextProvider>
       </body>
     </html>
   );
