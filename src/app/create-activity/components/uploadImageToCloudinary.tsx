034df04920c4578e91f2b2914910b43074b643c9
Lisandro85 <lisandrobedotti@hotmail.com>
2024-11-17 03:13:11 +0100

    random

---
+++ src/app/create-activity/components/uploadImageToCloudinary.tsx
@@ -0,0 +1,55 @@
+import { toast } from "react-toastify";
+
+export const UploadImageToCloudinary = async (file: File): Promise<string> => {
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET || '');
+  formData.append('cloud_name', process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || ''); 
+
+  try {
+    const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`, {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      toast.error(errorData.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        return '';
+    }else{
+      const result = await response.json();
+      toast.success(result.message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return result.secure_url;
+  }
+  } catch (error) {
+    const errorMessage = (error as any).message ? (error as any).message : "Error al subior la imagen";
+    toast.error(errorMessage, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    return '';
+  }
+};
+
+
