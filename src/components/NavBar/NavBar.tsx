0d6421b349677d88f784b6217e336de711a8bcea
Alexis-AFarias <aleexxfs99@gmail.com>
2024-11-17 22:49:24 -0300

    m

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -37,7 +37,7 @@ export default function NavBar() {
           {
             title: "Contacto",
             href: "/contact",
-          },
+          }
           {
             title: "Registro",
             href: "/register",
