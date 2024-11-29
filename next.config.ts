c7d573ace5248d5e1c7395799ebd7f1c68f13730
Chiara <chiaravalentinabosio@gmail.com>
2024-11-29 07:16:02 -0300

    cambios en next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignora errores de ESLint en la construcción
+  },
+  /* puedes agregar otras configuraciones aquí */
 };
 
 export default nextConfig;
