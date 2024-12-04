813c0306d09c9b76fdff280ff2fc51821df5b1f3
Alexis-AFarias <aleexxfs99@gmail.com>
2024-12-04 12:05:25 -0300

    hora de crear actividad y ajustes de perfil

--- src/app/create-activity/components/validationSchema.ts
+++ src/app/create-activity/components/validationSchema.ts
@@ -25,7 +25,36 @@ export const validationSchemaNewActivitie = Yup.object({
       return value >= today; 
     }
   ),
-  time: Yup.string().required("Requerido"),
+  time: Yup.string()
+  .required("Requerido")
+  .test(
+    "isFutureTime",
+    "La hora no puede ser anterior a la hora actual",
+    (value, context) => {
+      if (!value) return false;
+      const selectedDate = context.parent.date;
+      if (!selectedDate) return true;
+
+      const today = new Date();
+      const selectedDateTime = new Date(selectedDate);
+
+      if (selectedDateTime.setHours(0, 0, 0, 0) === today.setHours(0, 0, 0, 0)) {
+        
+        const now = new Date();
+        const currentHours = now.getHours();
+        const currentMinutes = now.getMinutes();
+        
+        const [selectedHours, selectedMinutes] = value.split(":").map(Number);
+        
+        if (selectedHours < currentHours || (selectedHours === currentHours && selectedMinutes < currentMinutes)) {
+          return false; 
+        }
+        
+        return true;
+      }
+      return true;
+    }
+  ),
   place: Yup.string().required("Requerido"),
   categoryId: Yup.string().required("Requerido"),
 });
